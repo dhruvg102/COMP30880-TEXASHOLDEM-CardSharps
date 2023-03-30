838f3917c1cedb9b61d67995f4d2cdaf0bf9962b
Murnane1 <conor.murnane@ucdconnect.ie>
2023-03-30 13:09:09 +0100

    holdem hand

---
+++ texasholdem/src/main/java/TexasHoldem/HoldemHand.java
@@ -0,0 +1,12 @@
+package TexasHoldem;
+
+import poker.*;
+
+public class HoldemHand extends PokerHand {
+
+    public HoldemHand(Card[] hand, DeckOfCards deck) {
+        super(hand, deck);
+    }
+
+
+}
