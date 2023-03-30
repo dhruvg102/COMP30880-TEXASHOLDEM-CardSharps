dc5455f75e7a2830067c58d7056a096fb0c97802
Eiree <eiresoftware@outlook.com>
2023-03-30 19:57:09 +0100

    Implemented test cases for hand classifications. Created enum for suit in hand and made each enum public for access. Fixed issues with isStraight() and now all are working correctly.

--- texasholdem/src/main/java/TexasHoldem/HoldemHand.java
+++ texasholdem/src/main/java/TexasHoldem/HoldemHand.java
@@ -1,100 +1,113 @@
 package TexasHoldem;
 import poker.*;
 
-//import java.lang.reflect.*;
 import java.util.Comparator;
 import java.util.Arrays;
 
+public class HoldemHand {
+    //enum for cardvalue, suit, risk and handvalue
+    public enum CardValue{
+        ACE(1, true),
+        DEUCE(2, false),
+        THREE(3, false),
+        FOUR(4, false),
+        FIVE(5, false),
+        SIX(6, false),
+        SEVEN(7, false),
+        EIGHT(8, false),
+        NINE(9, false),
+        TEN(10, false),
+        JACK(11, false),
+        QUEEN(12, false),
+        KING(13, false);
 
-//enumerators for handvalue, riskvalue and cardvalue
-enum CardValue{
-    ACE(1, true),
-    DEUCE(2, false),
-    THREE(3, false),
-    FOUR(4, false),
-    FIVE(5, false),
-    SIX(6, false),
-    SEVEN(7, false),
-    EIGHT(8, false),
-    NINE(9, false),
-    TEN(10, false),
-    JACK(11, false),
-    QUEEN(12,false),
-    KING(13, false);
+        private final int value;
+        private final boolean isAceHigh;
+        public static final int ACE_HIGH_VALUE = 14;
 
-    private final int value;
-    private final boolean isAceHigh;
-    public static final int ACE_HIGH_VALUE = 14;
+        private CardValue(int value, boolean isAceHigh){
+            this.value = value;
+            this.isAceHigh = isAceHigh;
+        }
 
-    private CardValue(int value, boolean isAceHigh){
-        this.value = value;
-        this.isAceHigh = isAceHigh;
+        public int getCardValue(boolean isAceHigh){
+            if (this == ACE) {
+                return isAceHigh ? ACE_HIGH_VALUE : 1;
+            } else {
+                return value;
+            }
+        }
+
+        public int getCardValue() {
+            return getCardValue(isAceHigh);
+        }
     }
 
-    
+    public enum Suit {
+        HEARTS("Hearts"),
+        DIAMONDS("Diamonds"),
+        CLUBS("Clubs"),
+        SPADES("Spades");
 
-    public int getCardValue(boolean isAceHigh){
-        if (this == ACE){
-            return isAceHigh ? ACE_HIGH_VALUE : 1;
-        } else {
+        private final String cardSuit;
+
+        private Suit(String cardSuit){
+            this.cardSuit = cardSuit;
+        }
+
+        @Override
+        public String toString(){
+            return cardSuit;
+        }
+    }
+
+    public enum HandValue {
+        ROYALFLUSH_VALUE(2000000000),
+        STRAIGHTFLUSH_VALUE(1000000000),
+        STRAIGHT_VALUE(100000000),
+        FOURS_VALUE(10000000),
+        FLUSH_VALUE(1000000),
+        FULLHOUSE_VALUE(100000),
+        THREES_VALUE(10000),
+        TWOPAIR_VALUE(1000),
+        PAIR_VALUE(100),
+        HIGHCARD_VALUE(0);
+
+        private final int value;
+
+        HandValue(int value){
+            this.value = value;
+        }
+
+        public int getHandValue(){
             return value;
         }
     }
 
-    public int getCardValue(){
-        return getCardValue(isAceHigh);
-    }
-}
+    public enum RiskWorthiness{
+        ROYALFLUSH_RISK(0),
+        STRAIGHTFLUSH_RISK(5),
+        STRAIGHT_RISK(10),
+        FOURS_RISK(15),
+        FLUSH_RISK(20),
+        FULLHOUSE_RISK(25),
+        THREES_RISK(30),
+        TWOPAIR_RISK(35),
+        PAIR_RISK(40),
+        HIGHCARD_RISK(90);
 
+        private final int risk;
 
-enum HandValue{
-    ROYALFLUSH_VALUE(2000000000),
-    STRAIGHTFLUSH_VALUE(1000000000),
-    STRAIGHT_VALUE(100000000),
-    FOURS_VALUE(10000000),
-    FLUSH_VALUE(1000000),
-    FULLHOUSE_VALUE(100000),
-    THREES_VALUE(10000),
-    TWOPAIR_VALUE(1000),
-    PAIR_VALUE(100),
-    HIGHCARD_VALUE(0);
+        RiskWorthiness(int risk){
+            this.risk = risk;
+        }
 
-    private final int value;
-    HandValue(int value){
-        this.value = value;
-    }
-
-    public int getHandValue(){
-        return value;
-    }
-}
-
-enum RiskWorthiness{
-    ROYALFLUSH_RISK(0),
-    STRAIGHTFLUSH_RISK(5),
-    STRAIGHT_RISK(10),
-    FOURS_RISK (15),
-    FLUSH_RISK(20),
-    FULLHOUSE_RISK(25),
-    THREES_RISK(30),
-    TWOPAIR_RISK(35),
-    PAIR_RISK(40),
-    HIGHCARD_RISK(90);
-
-    private final int risk;
-    RiskWorthiness(int risk) {
-        this.risk = risk;
+        public int getRiskValue(){
+            return risk;
+        }
     }
 
-    public int getRiskValue() {
-        return risk;
-    }
-}
 
-
-
-
-public class HoldemHand {
     public static final int DEFAULT_RISK = 20;
     public static final int NUMCARDS = 5; // total number of cards to be determined TODO - as players get 2 cards only their total hand is accounted for their best 5 cards.
     
@@ -104,7 +117,7 @@ public class HoldemHand {
 
     private int communityCardsCount; //counter for community cards
 
-    public HoldemHand(Card[] hand, DeckOfCards deck) {
+    public HoldemHand(Card[] hand, DeckOfCards deck){
         this.playerHand = hand; //init playerHand
         this.deck = deck; //init deck
     }
@@ -116,13 +129,13 @@ public class HoldemHand {
         communityCardsCount = 0;
     }
 
-    public int getRiskWorthiness() { //We override this value for specific hands such as Straight, FullHouse etc..
+    public int getRiskWorthiness(){ //We override this value for specific hands such as Straight, FullHouse etc..
         return DEFAULT_RISK;
     }
 
 
     //Display a hand
-    public String toString() {
+    public String toString(){
         String desc = "";
 
         for (int i = 0; i < NUMCARDS; i++)
@@ -133,7 +146,7 @@ public class HoldemHand {
 
 
     //modifiers
-    public void setCard(int num, Card card) {
+    public void setCard(int num, Card card){
         if (num >= 0 && num < NUMCARDS)
             playerHand[num] = card;
     }
@@ -141,14 +154,14 @@ public class HoldemHand {
 
 
     //Accesors
-    public Card getCard(int num) { //get card at index
+    public Card getCard(int num){ //get card at index
         if (num >= 0 && num < NUMCARDS)
             return playerHand[num];
         else
             return null;
     }
 
-    public int getValue() {
+    public int getValue(){
         return getCard(0).getValue(); // simply return the value of the higest card
     }
 
@@ -175,29 +188,38 @@ public class HoldemHand {
                 && playerHand[3].getValue() == playerHand[4].getValue();
     }
 
-    public boolean isStraight() { //returns true if all cards in the hand form a straight (values not suit)
+    public boolean isStraight(){
         sortHand();
-        for (int i = 0; i < NUMCARDS - 1; i++) {
-            if (playerHand[i].getValue() != playerHand[i + 1].getValue() - 1)
-                return false;
+        boolean isAceLow = playerHand[0].getValue() == CardValue.ACE.getCardValue(false);
+        int count = 0;
+        for (int i = 1; i < NUMCARDS; i++) {
+            if (playerHand[i].getValue() == playerHand[i - 1].getValue() - 1){
+                count++;
+            } else if (playerHand[i].getValue() == playerHand[i - 1].getValue()){
+                continue;
+            } else if (isAceLow && playerHand[i].getValue() == CardValue.FIVE.getCardValue()){
+                count++;
+            } else{
+                break;
+            }
         }
-        return true;
+        return count == 4;
     }
 
-    public boolean isFlush() { //returns true if all cards in the hand are the same suit
-        for (int i = 1; i < NUMCARDS; i++) {
-            if (getCard(i).getSuit() != getCard(0).getSuit()) {
+    public boolean isFlush(){ //returns true if all cards in the hand are the same suit
+        for (int i = 1; i < NUMCARDS; i++){
+            if (getCard(i).getSuit() != getCard(0).getSuit()){
                 return false;
             }
         }
         return true;
     }
 
-    public boolean isStraightFlush() { //returns true if hand is both a flush and a straight
-        return isFlush() && isStraightFlush();
+    public boolean isStraightFlush(){ //returns true if hand is both a flush and a straight
+        return isFlush() && isStraight();
     }
 
-    public boolean isRoyalFlush() { //returns true if the hand is a straight flush from 'Ten -> Ace'
+    public boolean isRoyalFlush(){ //returns true if the hand is a straight flush from 'Ten -> Ace'
         sortHand();
 
         /*
@@ -214,7 +236,7 @@ public class HoldemHand {
                 && playerHand[4].getValue() == CardValue.TEN.getCardValue();
     }
     
-    public boolean isThreeOfAKind() { //returns true if the hand has three cards of the same value
+    public boolean isThreeOfAKind(){ //returns true if the hand has three cards of the same value
         /* 
          If card 1 == card 3
          || card 2 == card 4
@@ -227,7 +249,7 @@ public class HoldemHand {
                 || playerHand[2].getValue() == playerHand[4].getValue();
     }
     
-    public boolean isTwoPair() { //returns true if the hand has two pairs of cards with the same values
+    public boolean isTwoPair(){ //returns true if the hand has two pairs of cards with the same values
         /*
         If card 1 == card 2 || card 3 == card 4 
         && card 4 == card 5 || card 2 == card 3 
@@ -243,24 +265,24 @@ public class HoldemHand {
                 && playerHand[3].getValue() == playerHand[4].getValue();
     }
     
-    public boolean isPair() { //returns true if the hand has one pair of cards with the same value
+    public boolean isPair(){ //returns true if the hand has one pair of cards with the same value
         sortHand();
-        for (int i = 0; i < NUMCARDS - 1; i++) {
+        for (int i = 0; i < NUMCARDS - 1; i++){
             if (playerHand[i].getValue() == playerHand[i + 1].getValue())
                 return true;
         }
         return false;
     }
     
-    public boolean isHigh() { //returns true if the hand has no classification
+    public boolean isHigh(){ //returns true if the hand has no classification
         sortHand(); //assuming that its sorted in desencding order when sorting. e.g ace will be defaulted to being 14 and will be sorted to the front
         if (isFlush() || isStraight() || isThreeOfAKind() || isTwoPair() || isPair() || isFullHouse()|| isFourOfAKind()){
             return false;
-        } else {
-            if (playerHand[0].getValue() == CardValue.ACE.getCardValue()) {
+        } else{
+            if (playerHand[0].getValue() == CardValue.ACE.getCardValue()){
                 CardValue.ACE.getCardValue(true);
                 return true; // hand has no classification - highest card is Ace
-            } else {
+            } else{
                 CardValue.ACE.getCardValue(false);
                 return true; // hand has no classification - highest card is !Ace
             }
@@ -268,44 +290,40 @@ public class HoldemHand {
     }
 
 
-    public void dealFlop() { //TODO - Im not sure how we're implementing this yet
+    public void dealFlop(){ //TODO - Im not sure how we're implementing this yet
         communityCards[0] = deck.dealNext();
         communityCards[1] = deck.dealNext();
         communityCards[2] = deck.dealNext();
         communityCardsCount = 3;
     }
 
-    public void dealTurn() {
+    public void dealTurn(){
         communityCards[3] = deck.dealNext();
         communityCardsCount = 4;
     }
 
-    public void dealRiver() {
+    public void dealRiver(){
         communityCards[4] = deck.dealNext();
         communityCardsCount = 5;
+    }
+
+    public void showDown() {
+        //
     }
 
     // showdown ->
     // for each player -> player.setBestHand based on a getBestHand method which
     // takes community cards into account.
 
-    public void showDown() {
-        //
-    }
-
-    private void sortHand() {
-        Arrays.sort(playerHand, new Comparator<Card>() {
+    private void sortHand(){
+        Arrays.sort(playerHand, new Comparator<Card>(){
             @Override
-            public int compare(Card card1, Card card2) {
+            public int compare(Card card1, Card card2){
                 return Integer.compare(card2.getValue(), card1.getValue()); // sort in descending order
             }
         });
     }
 
-
-    //Sorting/catergorize the hand will probably be needed for hand classification to simplify going through each hand - I dont think TODO
-    //sort hand -> pokerhand
-    //categorize hand -> pokerhand
-
     //Sorting should be done in desending order, otherwise we will need to change the above classifications code
+    //unsure on catergorization?
 }
--- texasholdem/src/test/java/texasholdem/HoldemHandTest.java
+++ texasholdem/src/test/java/texasholdem/HoldemHandTest.java
@@ -4,28 +4,213 @@ import static org.junit.Assert.*;
 
 import poker.*;
 import TexasHoldem.HoldemHand;
+import TexasHoldem.HoldemHand.CardValue;
+import TexasHoldem.HoldemHand.Suit;
 
 public class HoldemHandTest {
+
+    /*
+     * WORKING TESTS --- NOTE: Cards are sorted in order of their value using the handSort() method in HoldemHand.
+     * FOUR OF A KIND
+     * THREE OF A KIND
+     * PAIR
+     * FULL HOUSE
+     * FLUSH
+     * ROYAL FLUSH
+     * HIGH
+     * STRAIGHT
+     * STRAIGHT FLUSH
+     */
+
     @Test
-    public void testIsFourOfAKind() {
-        // Test for matching handvb
-        HoldemHand hand = new HoldemHand(new Card[] {
-                new NumberCard("Ace", "SUIT", 14),
-                new NumberCard("Ace", "SUIT", 14),
-                new NumberCard("Ace", "SUIT", 14),
-                new NumberCard("Ace", "SUIT", 14),
-                new NumberCard("King", "SUIT", 13),
+    public void testIsFourOfAKind() { 
+        // TEST TRUE
+        HoldemHand hand = new HoldemHand(new Card[]{
+                new NumberCard("Ace", Suit.HEARTS.toString(), CardValue.ACE.getCardValue()),
+                new NumberCard("Ace", Suit.SPADES.toString(), CardValue.ACE.getCardValue()),
+                new NumberCard("Ace", Suit.CLUBS.toString(), CardValue.ACE.getCardValue()),
+                new NumberCard("Ace", Suit.DIAMONDS.toString(), CardValue.ACE.getCardValue()),
+                new FaceCard("King", Suit.HEARTS.toString(), CardValue.KING.getCardValue()),
         }, new DeckOfCards());
         assertTrue(hand.isFourOfAKind());
 
-        // Test for non-matching hand
-        hand = new HoldemHand(new Card[] {
-                new NumberCard("Ace", "SUIT", 14),
-                new NumberCard("Ace", "SUIT", 14),
-                new NumberCard("Ace", "SUIT", 14),
-                new NumberCard("King", "SUIT", 13),
-                new NumberCard("King", "SUIT", 13),
+        // TEST FALSE
+        hand = new HoldemHand(new Card[]{
+                new NumberCard("Ace", Suit.CLUBS.toString(), CardValue.ACE.getCardValue()),
+                new NumberCard("Ace", Suit.SPADES.toString(), CardValue.ACE.getCardValue()),
+                new NumberCard("Ace", Suit.CLUBS.toString(), CardValue.ACE.getCardValue()),
+                new NumberCard("King", Suit.DIAMONDS.toString(), CardValue.KING.getCardValue()),
+                new FaceCard("King", Suit.CLUBS.toString(), CardValue.KING.getCardValue()),
         }, new DeckOfCards());
         assertFalse(hand.isFourOfAKind());
+    }
+
+    @Test
+    public void testIsThreeOfAKind() {
+        HoldemHand hand = new HoldemHand(new Card[]{
+                new NumberCard("Ace", Suit.HEARTS.toString(), CardValue.ACE.getCardValue()),
+                new NumberCard("Ace", Suit.CLUBS.toString(), CardValue.ACE.getCardValue()),
+                new NumberCard("Ace", Suit.HEARTS.toString(), CardValue.ACE.getCardValue()),
+                new FaceCard("King", Suit.DIAMONDS.toString(), CardValue.KING.getCardValue()),
+                new FaceCard("Queen", Suit.DIAMONDS.toString(), CardValue.QUEEN.getCardValue()),
+        }, new DeckOfCards());
+        assertTrue(hand.isThreeOfAKind());
+
+        hand = new HoldemHand(new Card[]{
+                new NumberCard("Ace", Suit.HEARTS.toString(), CardValue.ACE.getCardValue()),
+                new NumberCard("Ace", Suit.CLUBS.toString(), CardValue.ACE.getCardValue()),
+                new NumberCard("Ten", Suit.HEARTS.toString(), CardValue.TEN.getCardValue()),
+                new FaceCard("King", Suit.DIAMONDS.toString(), CardValue.KING.getCardValue()),
+                new FaceCard("Queen", Suit.DIAMONDS.toString(), CardValue.QUEEN.getCardValue()),
+        }, new DeckOfCards());
+        assertFalse(hand.isThreeOfAKind());
+    }
+
+    @Test
+    public void isPair() {
+        HoldemHand hand = new HoldemHand(new Card[]{
+                new NumberCard("Ace", Suit.HEARTS.toString(), CardValue.ACE.getCardValue()),
+                new NumberCard("Ace", Suit.CLUBS.toString(), CardValue.ACE.getCardValue()),
+                new NumberCard("Ten", Suit.HEARTS.toString(), CardValue.TEN.getCardValue()),
+                new FaceCard("King", Suit.DIAMONDS.toString(), CardValue.KING.getCardValue()),
+                new FaceCard("Queen", Suit.DIAMONDS.toString(), CardValue.QUEEN.getCardValue()),
+        }, new DeckOfCards());
+        assertTrue(hand.isPair());
+
+        hand = new HoldemHand(new Card[]{
+                new NumberCard("Ace", Suit.HEARTS.toString(), CardValue.ACE.getCardValue()),
+                new NumberCard("Three", Suit.CLUBS.toString(), CardValue.THREE.getCardValue()),
+                new NumberCard("Ten", Suit.HEARTS.toString(), CardValue.TEN.getCardValue()),
+                new FaceCard("King", Suit.DIAMONDS.toString(), CardValue.KING.getCardValue()),
+                new FaceCard("Queen", Suit.DIAMONDS.toString(), CardValue.QUEEN.getCardValue()),
+        }, new DeckOfCards());
+        assertFalse(hand.isPair());
+    }
+
+    @Test
+    public void testIsFullHouse(){
+        HoldemHand hand = new HoldemHand(new Card[]{
+                new NumberCard("Ace", Suit.HEARTS.toString(), CardValue.ACE.getCardValue()),
+                new NumberCard("Ace", Suit.CLUBS.toString(), CardValue.ACE.getCardValue()),
+                new NumberCard("Ace", Suit.HEARTS.toString(), CardValue.ACE.getCardValue()),
+                new FaceCard("King", Suit.CLUBS.toString(), CardValue.KING.getCardValue()),
+                new FaceCard("King", Suit.DIAMONDS.toString(), CardValue.KING.getCardValue()),
+        }, new DeckOfCards());
+        assertTrue(hand.isFullHouse());
+
+        hand = new HoldemHand(new Card[]{
+                new NumberCard("Ace", Suit.HEARTS.toString(), CardValue.ACE.getCardValue()),
+                new NumberCard("Ace", Suit.CLUBS.toString(), CardValue.ACE.getCardValue()),
+                new NumberCard("Ace", Suit.HEARTS.toString(), CardValue.ACE.getCardValue()),
+                new FaceCard("King", Suit.DIAMONDS.toString(), CardValue.KING.getCardValue()),
+                new FaceCard("Queen", Suit.DIAMONDS.toString(), CardValue.QUEEN.getCardValue()),
+        }, new DeckOfCards());
+        assertFalse(hand.isFullHouse());
+    }
+
+    @Test
+    public void isFlush(){
+        HoldemHand hand = new HoldemHand(new Card[]{
+                    new NumberCard("Ace", Suit.DIAMONDS.toString(), CardValue.ACE.getCardValue()),
+                    new FaceCard("King", Suit.DIAMONDS.toString(), CardValue.KING.getCardValue()),
+                    new FaceCard("Queen", Suit.DIAMONDS.toString(), CardValue.QUEEN.getCardValue()),
+                    new NumberCard("Ten", Suit.DIAMONDS.toString(), CardValue.TEN.getCardValue()),
+                    new NumberCard("Nine", Suit.DIAMONDS.toString(), CardValue.NINE.getCardValue()),
+        },new DeckOfCards());
+        assertTrue(hand.isFlush());
+
+        hand = new HoldemHand(new Card[]{
+                new NumberCard("Ace", Suit.DIAMONDS.toString(), CardValue.ACE.getCardValue()),
+                new FaceCard("King", Suit.DIAMONDS.toString(), CardValue.KING.getCardValue()),
+                new FaceCard("Queen", Suit.DIAMONDS.toString(), CardValue.QUEEN.getCardValue()),
+                new NumberCard("Ten", Suit.CLUBS.toString(), CardValue.TEN.getCardValue()),
+                new NumberCard("Nine", Suit.HEARTS.toString(), CardValue.NINE.getCardValue()),
+        }, new DeckOfCards());
+        assertFalse(hand.isFlush());
+    }
+
+    @Test
+    public void isRoyalFlush(){
+        HoldemHand hand = new HoldemHand(new Card[] {
+                new NumberCard("Ace", Suit.DIAMONDS.toString(), CardValue.ACE.getCardValue()),
+                new FaceCard("King", Suit.DIAMONDS.toString(), CardValue.KING.getCardValue()),
+                new FaceCard("Queen", Suit.DIAMONDS.toString(), CardValue.QUEEN.getCardValue()),
+                new FaceCard("Jack", Suit.DIAMONDS.toString(), CardValue.JACK.getCardValue()),
+                new NumberCard("Ten", Suit.DIAMONDS.toString(), CardValue.TEN.getCardValue()),
+        }, new DeckOfCards());
+        assertTrue(hand.isRoyalFlush());
+
+        hand = new HoldemHand(new Card[]{
+                new NumberCard("Ace", Suit.DIAMONDS.toString(), CardValue.ACE.getCardValue()),
+                new FaceCard("King", Suit.CLUBS.toString(), CardValue.KING.getCardValue()),
+                new FaceCard("Queen", Suit.HEARTS.toString(), CardValue.QUEEN.getCardValue()),
+                new NumberCard("Nine", Suit.DIAMONDS.toString(), CardValue.NINE.getCardValue()),
+                new NumberCard("Three", Suit.DIAMONDS.toString(), CardValue.THREE.getCardValue()),
+        }, new DeckOfCards());
+        assertFalse(hand.isRoyalFlush());
+    }
+
+    @Test
+    public void isHigh(){
+        HoldemHand hand = new HoldemHand(new Card[]{
+                new NumberCard("Ace", Suit.DIAMONDS.toString(), CardValue.ACE.getCardValue()),
+                new FaceCard("King", Suit.HEARTS.toString(), CardValue.KING.getCardValue()),
+                new NumberCard("Ten", Suit.DIAMONDS.toString(), CardValue.TEN.getCardValue()),
+                new NumberCard("Six", Suit.SPADES.toString(), CardValue.SIX.getCardValue()),
+                new NumberCard("Four", Suit.CLUBS.toString(), CardValue.FOUR.getCardValue()),
+        }, new DeckOfCards());
+        assertTrue(hand.isHigh());
+
+        hand = new HoldemHand(new Card[]{
+                new NumberCard("Ace", Suit.DIAMONDS.toString(), CardValue.ACE.getCardValue()),
+                new NumberCard("Ace", Suit.HEARTS.toString(), CardValue.ACE.getCardValue()),
+                new NumberCard("Ten", Suit.DIAMONDS.toString(), CardValue.TEN.getCardValue()),
+                new NumberCard("Six", Suit.SPADES.toString(), CardValue.SIX.getCardValue()),
+                new NumberCard("Four", Suit.CLUBS.toString(), CardValue.FOUR.getCardValue()),
+        }, new DeckOfCards());
+        assertFalse(hand.isHigh());
+    }
+
+    @Test
+    public void isStraight(){
+        HoldemHand hand = new HoldemHand(new Card[]{
+                new NumberCard("Eight", Suit.DIAMONDS.toString(), CardValue.EIGHT.getCardValue()),
+                new NumberCard("Seven", Suit.HEARTS.toString(), CardValue.SEVEN.getCardValue()),
+                new NumberCard("Six", Suit.DIAMONDS.toString(), CardValue.SIX.getCardValue()),
+                new NumberCard("Five", Suit.SPADES.toString(), CardValue.FIVE.getCardValue()),
+                new NumberCard("Four", Suit.CLUBS.toString(), CardValue.FOUR.getCardValue()),
+        }, new DeckOfCards());
+        assertTrue(hand.isStraight());
+
+        hand = new HoldemHand(new Card[]{
+                new FaceCard("Queen", Suit.DIAMONDS.toString(), CardValue.QUEEN.getCardValue()),
+                new NumberCard("Ten", Suit.HEARTS.toString(), CardValue.TEN.getCardValue()),
+                new NumberCard("Eight", Suit.DIAMONDS.toString(), CardValue.EIGHT.getCardValue()),
+                new NumberCard("Five", Suit.SPADES.toString(), CardValue.FIVE.getCardValue()),
+                new NumberCard("Four", Suit.CLUBS.toString(), CardValue.FOUR.getCardValue()),
+        }, new DeckOfCards());
+        assertFalse(hand.isStraight());
+    }
+
+
+    @Test
+    public void isStraightFlush(){
+        HoldemHand hand = new HoldemHand(new Card[]{
+                new NumberCard("Ace", Suit.DIAMONDS.toString(), CardValue.ACE.getCardValue()),
+                new FaceCard("King", Suit.DIAMONDS.toString(), CardValue.KING.getCardValue()),
+                new FaceCard("Queen", Suit.DIAMONDS.toString(), CardValue.QUEEN.getCardValue()),
+                new FaceCard("Jack", Suit.DIAMONDS.toString(), CardValue.JACK.getCardValue()),
+                new NumberCard("Ten", Suit.DIAMONDS.toString(), CardValue.TEN.getCardValue()),
+        }, new DeckOfCards());
+        assertTrue(hand.isStraightFlush());
+
+        hand = new HoldemHand(new Card[]{
+                new NumberCard("Ace", Suit.DIAMONDS.toString(), CardValue.ACE.getCardValue()),
+                new FaceCard("King", Suit.CLUBS.toString(), CardValue.KING.getCardValue()),
+                new FaceCard("Queen", Suit.DIAMONDS.toString(), CardValue.QUEEN.getCardValue()),
+                new FaceCard("Jack", Suit.HEARTS.toString(), CardValue.JACK.getCardValue()),
+                new NumberCard("Ten", Suit.DIAMONDS.toString(), CardValue.TEN.getCardValue()),
+        }, new DeckOfCards());
+        assertFalse(hand.isStraightFlush());
     }
 }
