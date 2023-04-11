8a42bc8cbdfa1e4801517f11415fc2be89866d14
noxus55 <ryan.daly@ucdconnect.ie>
2023-04-11 05:06:45 +0100

    Completed analysis for post-flop cards

--- texasholdem/src/main/java/TexasHoldem/PostFlopHandAnalyser.java
+++ texasholdem/src/main/java/TexasHoldem/PostFlopHandAnalyser.java
@@ -9,20 +9,6 @@ TODO ensure hold cards are preserved in position 0 and 1 in HoldemHand class
  */
 
 public class PostFlopHandAnalyser {
-    private enum handType {
-        MONSTER,
-        OVERPAIR,
-        STRONGTWOPAIR,
-        WEAKTWOPAIR,
-        MONSTERDRAW,
-        OESD,
-        FDRAW,
-        DBGUNSHOT,
-        GUTSHOT,
-        OVERCARDS,
-        TRASH
-    }
-
     private HoldemHand hand;
     private ArrayList<Card> holdCards = new ArrayList<Card>();
 
@@ -42,7 +28,21 @@ public class PostFlopHandAnalyser {
         this.mergedHand.addAll(communityCards);
     }
 
-    private boolean isTopPair() {
+    public void sortMergedHandAscending() {
+        Card temp;
+
+        for(int i = 0; i < mergedHand.size() - 1; i++) {
+            for(int j = 0; j < mergedHand.size() - i - 1; j++) {
+                if(mergedHand.get(j).getValue() > mergedHand.get(j + 1).getValue()) {
+                    temp = mergedHand.get(j);
+                    mergedHand.set(j, mergedHand.get(j + 1));
+                    mergedHand.set(j + 1, temp);
+                }
+            }
+        }
+    }
+
+    public boolean isTopPair() {
         boolean cardIsEqual = false;
 
         for(int i = 0; i < holdCards.size(); i++) {
@@ -56,7 +56,7 @@ public class PostFlopHandAnalyser {
         return cardIsEqual;
     }
 
-    private boolean isOverPair() {
+    public boolean isOverPair() {
         boolean cardIsHighest = true;
 
         for(int i = 0; i < holdCards.size(); i++) {
@@ -67,10 +67,14 @@ public class PostFlopHandAnalyser {
             }
         }
 
+        if(!(holdCards.get(0).getValue() == holdCards.get(1).getValue())) {
+            cardIsHighest = false;
+        }
+
         return cardIsHighest;
     }
 
-    private boolean isWeakTwoPair() {
+    public boolean isWeakTwoPair() {
         boolean holdPairFound = false;
         boolean communityPairFound = false;
 
@@ -93,7 +97,7 @@ public class PostFlopHandAnalyser {
         return holdPairFound && communityPairFound;
     }
 
-    private boolean isStrongTwoPair() {
+    public boolean isStrongTwoPair() {
         boolean firstPairFound = false;
         boolean secondPairFound = false;
 
@@ -111,7 +115,210 @@ public class PostFlopHandAnalyser {
         return firstPairFound && secondPairFound;
     }
 
-    private boolean isMonsterHand() {
-        return (isStrongTwoPair() || (hand.evaluateHand(mergedHand) >= 10000));//hand.HandValue.THREES_VALUE.getHandValue()) );
+    public boolean isMonsterHand() {
+        return (isStrongTwoPair() || (hand.evaluateHand(mergedHand) >= 10000));//TODO hand.HandValue.THREES_VALUE.getHandValue()) );
+    }
+
+    public boolean isOESD() {
+        int OESDCount = 4;
+        int runningSequence = 0;
+        int previousValue = -1;
+        int currentValue;
+
+        if(hand.evaluateHand(mergedHand) != 100000000) { //TODO value of straight
+            sortMergedHandAscending();
+
+            for(int i = 0; i < mergedHand.size(); i++){
+                currentValue = mergedHand.get(i).getValue();
+
+                if(currentValue == previousValue + 1){
+                    runningSequence++;
+                }
+                else {
+                    runningSequence = 0;
+                }
+
+                previousValue = currentValue;
+            }
+        }
+
+        return runningSequence == OESDCount;
+    }
+
+    private boolean isOESD(int[] cardValues) {
+        int OESDCount = 4;
+        int runningSequence = 0;
+        int previousValue = -1;
+        int currentValue;
+
+        if(hand.evaluateHand(mergedHand) != 100000000) { //TODO value of straight
+
+            for(int i = 0; i < cardValues.length; i++){
+                currentValue = cardValues[i];
+
+                if(currentValue == previousValue + 1){
+                    runningSequence++;
+                }
+                else {
+                    runningSequence = 0;
+                }
+
+                previousValue = currentValue;
+            }
+        }
+
+        return runningSequence == OESDCount;
+    }
+
+    public boolean isGutshot() {
+        boolean gutshot = false;
+        int[] mergedHandValues = new int[mergedHand.size()];
+        int[] temp;
+
+        sortMergedHandAscending();
+
+        for (int i = 0; i < mergedHand.size(); i++) {
+            mergedHandValues[i] = mergedHand.get(i).getValue();
+        }
+        temp = mergedHandValues;
+
+        if (hand.evaluateHand(mergedHand) != 100000000 && !(isGutshot())) { //TODO value of straight)
+            for (int i = 0; i < mergedHand.size(); i++) {
+                for (int j = 2; j <= 14; j++) {
+                    mergedHandValues[i] = j;
+
+                    if (isOESD(mergedHandValues)) {
+                        gutshot = true;
+                    }
+                }
+                mergedHandValues = temp;
+            }
+        }
+
+        return gutshot;
+    }
+
+    public boolean isDBGutshot() {
+        boolean gutshot = false;
+        boolean dbGutshot = false;
+        int[] mergedHandValues = new int[mergedHand.size()];
+        int[] temp;
+        int[] reducedHand;
+        int gutshotIndex = 0;
+
+        sortMergedHandAscending();
+
+        for (int i = 0; i < mergedHand.size(); i++) {
+            mergedHandValues[i] = mergedHand.get(i).getValue();
+        }
+        temp = mergedHandValues;
+
+        if (hand.evaluateHand(mergedHand) != 100000000 && !(isGutshot())) { //TODO value of straight)
+            for (int i = 0; i < mergedHand.size(); i++) {
+                int tempGutshotIndex = i;
+                for (int j = 2; j <= 14; j++) {
+                    mergedHandValues[i] = j;
+
+                    if (isOESD(mergedHandValues)) {
+                        gutshot = true;
+                        gutshotIndex = tempGutshotIndex;
+                    }
+                }
+                mergedHandValues = temp;
+            }
+
+            reducedHand = new int[mergedHand.size() - gutshotIndex + 1];
+
+            int tempIndex = 0;
+            for(int i = gutshotIndex; i < mergedHand.size(); i ++) {
+                    reducedHand[tempIndex++] = mergedHand.get(i).getValue();
+            }
+
+            temp = reducedHand;
+
+            if (gutshot) {
+                for (int i = gutshotIndex; i < reducedHand.length; i++) {
+                    for (int j = 2; j <= 14; j++) {
+                        reducedHand[i] = j;
+
+                        if (isOESD(reducedHand)) {
+                            dbGutshot = true;
+                        }
+                    }
+                    reducedHand = temp;
+                }
+            }
+        }
+
+        return dbGutshot;
+    }
+
+    public boolean isFlushDraw() { //TODO add case for monochrome community cards
+        int heartCount = 0;
+        int spadeCount = 0;
+        int clubCount = 0;
+        int diamondCount = 0;
+
+        boolean flushDraw = false;
+
+        for(int i = 0; i < mergedHand.size(); i ++){
+            switch (mergedHand.get(i).getSuit()) {
+                case "hearts":
+                    heartCount++;
+                case "spades":
+                    spadeCount++;
+                case "clubs":
+                    clubCount++;
+                case "diamonds":
+                    diamondCount++;
+            }
+        }
+
+        if(heartCount == 4 || spadeCount == 4 || clubCount == 4 || diamondCount == 4 ) {
+            flushDraw = true;
+        }
+
+        return flushDraw;
+    }
+
+    public boolean isMonsterDraw() {
+        if(isFlushDraw() && (isOESD() || isGutshot() || isTopPair())) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean isOverCard() {
+        boolean cardIsHighest = true;
+
+        for(int i = 0; i < holdCards.size(); i++) {
+            for(int j = 0; j < communityCards.size(); j++) {
+                if(holdCards.get(i).getValue() <= communityCards.get(j).getValue()) {
+                    cardIsHighest = false;
+                }
+            }
+        }
+
+        return cardIsHighest;
+    }
+
+    public boolean isTrash() {
+        boolean trash = true;
+
+        if(isOESD() || isGutshot() || isDBGutshot() || isFlushDraw() || isMonsterDraw()){
+            trash = false;
+        }
+
+        if(isTopPair() || isOverPair() || isWeakTwoPair() || isStrongTwoPair() || isMonsterHand()){
+            trash = false;
+        }
+
+        if(isOverCard()){
+            trash = false;
+        }
+
+        return trash;
     }
 }
