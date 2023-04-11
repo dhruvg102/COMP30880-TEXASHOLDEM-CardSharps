20e37710ca7f19ea86e35bf527e39b9c657bba6d
noxus55 <ryan.daly@ucdconnect.ie>
2023-04-11 09:19:36 +0100

    Implemented algorithm for deciding next move post-flop

--- texasholdem/src/main/java/TexasHoldem/PostFlopMoveAnalyser.java
+++ texasholdem/src/main/java/TexasHoldem/PostFlopMoveAnalyser.java
@@ -7,6 +7,16 @@ public class PostFlopMoveAnalyser {
         RIVER
     }
 
+    private enum holdemMoves {
+        BET,
+        CHECK,
+        FOLD,
+        CONTINUATIONBET,
+        CALL,
+        ALLIN,
+        RAISE,
+    }
+
     PostFlopHandAnalyser handAnalyser;
 
     private boolean playerHasBet = false;
@@ -15,9 +25,143 @@ public class PostFlopMoveAnalyser {
 
     private boolean opponentHasBet = false;
 
+    private boolean opponentHasRaisedAfterPlayer = false;
+
+    private int numberOfPlayers;
+
     private gameStage stage = gameStage.FLOP;
 
-    public PostFlopMoveAnalyser(HoldemHand hand) {
+    private holdemMoves nextMove = holdemMoves.FOLD;
+
+    public PostFlopMoveAnalyser(HoldemHand hand, int numberOfPlayers) {
         this.handAnalyser = new PostFlopHandAnalyser(hand);
+        this.numberOfPlayers = numberOfPlayers;
+    }
+
+    public void decideNextMove() {
+        if(stage == gameStage.FLOP) {
+            if(handAnalyser.isDry() && numberOfPlayers == 2) {
+                if(handAnalyser.isMonsterHand() || handAnalyser.isOverPair() || handAnalyser.isMonsterDraw()) {
+                    if(opponentHasBet) {
+                        nextMove = holdemMoves.RAISE;
+                    }
+                    else {
+                        nextMove = holdemMoves.BET;
+                    }
+                }
+                else {
+                    if(opponentHasBet) {
+                        nextMove = holdemMoves.FOLD;
+                    }
+                    else {
+                        nextMove = holdemMoves.CONTINUATIONBET;
+                    }
+                }
+            }
+            else if(handAnalyser.isDrawy() || numberOfPlayers > 2) {
+                if(handAnalyser.isMonsterHand() || handAnalyser.isOverPair() || handAnalyser.isMonsterDraw()) {
+                    if(opponentHasBet) {
+                        nextMove = holdemMoves.RAISE;
+                    }
+                    else {
+                        nextMove = holdemMoves.BET;
+                    }
+                }
+                else if(handAnalyser.isGutshot() || handAnalyser.isOverCard() || handAnalyser.isTrash()) {
+                    if(opponentHasBet) {
+                        nextMove = holdemMoves.FOLD;
+                    }
+                    else {
+                        nextMove = holdemMoves.CHECK;
+                    }
+                }
+                else {
+                    if(opponentHasBet) {
+                        nextMove = holdemMoves.FOLD;
+                    }
+                    else {
+                        nextMove = holdemMoves.CONTINUATIONBET;
+                    }
+                }
+            }
+        }
+        else if(stage == gameStage.TURN) {
+            if(handAnalyser.isMonsterHand() || handAnalyser.isOverPair() || handAnalyser.isMonsterDraw()) {
+                if(opponentHasRaisedAfterPlayer) {
+                    nextMove = holdemMoves.ALLIN;
+                }
+                else if(opponentHasBet) {
+                    nextMove = holdemMoves.RAISE;
+                }
+                else {
+                    nextMove = holdemMoves.BET;
+                }
+            }
+            else if(handAnalyser.isTopPair()) {
+                if(opponentHasRaisedAfterPlayer || opponentHasBet) {
+                    nextMove = holdemMoves.FOLD;
+                }
+                else {
+                    nextMove = holdemMoves.CONTINUATIONBET;
+                }
+            }
+            else {
+                if(opponentHasBet) {
+                    nextMove = holdemMoves.FOLD;
+                }
+                else {
+                    nextMove = holdemMoves.CHECK;
+                }
+            }
+        }
+        else if(stage == gameStage.RIVER) {
+            if(playerHasBet) {
+                if(handAnalyser.isMonsterHand() || handAnalyser.isOverPair() || handAnalyser.isTopPair()) {
+                    if(opponentHasRaisedAfterPlayer) {
+                        nextMove = holdemMoves.ALLIN;
+                    }
+                    else if(opponentHasBet) {
+                        nextMove = holdemMoves.RAISE;
+                    }
+                    else {
+                        nextMove = holdemMoves.BET;
+                    }
+                }
+                else{
+                    if(opponentHasBet) {
+                        nextMove = holdemMoves.FOLD;
+                    }
+                    else {
+                        nextMove = holdemMoves.CHECK;
+                    }
+                }
+            }
+            else if(playerHasChecked) {
+                if(handAnalyser.isMonsterHand()) {
+                    if(opponentHasRaisedAfterPlayer) {
+                        nextMove = holdemMoves.ALLIN;
+                    }
+                    else if(opponentHasBet) {
+                        nextMove = holdemMoves.RAISE;
+                    }
+                    else {
+                        nextMove = holdemMoves.BET;
+                    }
+                }
+                else if(handAnalyser.isTopPair()) {
+                    if(opponentHasRaisedAfterPlayer || opponentHasBet) {
+                        nextMove = holdemMoves.CALL;
+                    }
+                }
+                else {
+                    if(opponentHasBet) {
+                        nextMove = holdemMoves.FOLD;
+                    }
+                    else {
+                        nextMove = holdemMoves.CHECK;
+                    }
+                }
+            }
+        }
     }
 }
